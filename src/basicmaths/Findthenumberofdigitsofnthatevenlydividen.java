6a44610b10cdfa3ce4b757cf2e457b0cc7b61d90
burrasaiteja21 <burra.saiteja21@gmail.com>
2023-09-23 22:40:42 +0530

    basic commit

---
+++ src/basicmaths/Findthenumberofdigitsofnthatevenlydividen.java
@@ -0,0 +1,28 @@
+package basicmaths;
+
+public class Findthenumberofdigitsofnthatevenlydividen {
+    public static void main(String args[])
+    {
+        int n=336;
+        System.out.println(countDigits(n)+" ");
+    }
+    public static int countDigits(int n)
+    {
+
+        int count=0;
+        int value=n;
+        while(value>0)
+        {
+            int rem=value%10;
+            if(rem!=0)
+            {
+                if(n%rem==0)
+                {
+                    count++;
+                }
+            }
+            value=value/10;
+        }
+        return count;
+    }
+}
